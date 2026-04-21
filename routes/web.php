42c94eabfd6ae46f4dc8ae1caea7bde884100bd1
RafkaJulianee <rafkagenggkk@gmail.com>
2026-04-21 21:58:45 +0700

    Update Login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/login', function () {
+    return view('login.login');
+})->name('login');
+
+Route::post('/login', function () {
+    /* 
+    | Bagian ini dapat diganti dengan AuthController untuk menangani logika login.
+    | Frontend sudah siap menerima session 'success' atau 'error' 
+    | untuk menampilkan notifikasi toast.
+    */
+    return redirect()->back()->with('error', 'Fitur login sedang disiapkan oleh tim backend.');
+});
