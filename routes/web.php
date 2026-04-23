2a14380ddd8e849407f0a3d2c82a03ebdee7a1fe
RafkaJulianee <rafkagenggkk@gmail.com>
2026-04-23 19:57:49 +0700

    feat: update landing page design - icons, layout, footer

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,22 @@ Route::get('/', function () {
 });
 
 Route::get('/login', function () {
-    return view('login.login');
+    return view('auth.login');
 })->name('login');
 
+
+Route::get('/dashboard', function () {
+    return view('costumer_service.dashboard');
+})->name('dashboard');
+
 Route::post('/login', function () {
-    /* 
-    | Bagian ini dapat diganti dengan AuthController untuk menangani logika login.
-    | Frontend sudah siap menerima session 'success' atau 'error' 
-    | untuk menampilkan notifikasi toast.
-    */
-    return redirect()->back()->with('error', 'Fitur login sedang disiapkan oleh tim backend.');
+ 
+    return redirect()->route('dashboard');
+});
+Route::post('/logout', function () {
+    return redirect()->route('login');
+})->name('logout');
+
+Route::get('/logout', function () {
+    return redirect()->route('login');
 });
